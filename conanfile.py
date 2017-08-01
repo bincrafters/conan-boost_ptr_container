0e7d8eca1b829690b083fd0fb54900694ea934b4
Jerry Wiltse <jerrywiltse@gmail.com>
2017-08-01 17:29:20 -0400

    added package info header-only flag

--- conanfile.py
+++ conanfile.py
@@ -5,14 +5,33 @@ class BoostPtr_ContainerConan(ConanFile):
     version = "1.64.0"
     generators = "txt"
     url = "https://github.com/boostorg/ptr_container"
-    description = "For a description of this library, please visit http://boost.org/ptr_container "
+    description = "Please visit http://www.boost.org/doc/libs/1_64_0/libs/libraries.htm"
     license = "www.boost.org/users/license.html"
     lib_short_name = "ptr_container"
+    requires =  "Boost.Array/1.64.0@bincrafters/testing", \
+                      "Boost.Assert/1.64.0@bincrafters/testing", \
+                      "Boost.Circular_Buffer/1.64.0@bincrafters/testing", \
+                      "Boost.Config/1.64.0@bincrafters/testing", \
+                      "Boost.Core/1.64.0@bincrafters/testing", \
+                      "Boost.Iterator/1.64.0@bincrafters/testing", \
+                      "Boost.Mpl/1.64.0@bincrafters/testing", \
+                      "Boost.Range/1.64.0@bincrafters/testing", \
+                      "Boost.Serialization/1.64.0@bincrafters/testing", \
+                      "Boost.Smart_Ptr/1.64.0@bincrafters/testing", \
+                      "Boost.Static_Assert/1.64.0@bincrafters/testing", \
+                      "Boost.Type_Traits/1.64.0@bincrafters/testing", \
+                      "Boost.Unordered/1.64.0@bincrafters/testing", \
+                      "Boost.Utility/1.64.0@bincrafters/testing"
 
+                      #array3 assert1 circular_buffer8 config0 core2 iterator5 mpl5 range7 serialization11 smart_ptr4 static_assert1 type_traits3 unordered8 utility5
+                      
     def source(self):
         self.run("git clone --depth=50 --branch=boost-{0} {1}.git"
                  .format(self.version, self.url))
 
     def package(self):
-        include_dir = src=os.path.join(os.getcwd(), self.lib_short_name, "include")
+        include_dir = os.path.join(self.build_folder, self.lib_short_name, "include")
         self.copy(pattern="*", dst="", src=include_dir)
+
+    def package_id(self):
+        self.info.header_only()
