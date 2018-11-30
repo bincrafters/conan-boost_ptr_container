932ead6a613896c384b94f07f1d7ea978dff0781
Rene Rivera <grafikrobot@gmail.com>
2018-11-29 22:01:12 -0600

    Initial 1.68.0 [skip ci]

--- conanfile.py
+++ conanfile.py
@@ -4,7 +4,7 @@
 from conans import python_requires
 
 
-base = python_requires("boost_base/1.67.0@bincrafters/testing")
+base = python_requires("boost_base/1.68.0@bincrafters/testing")
 
 class BoostPtr_ContainerConan(base.BoostBaseConan):
     name = "boost_ptr_container"
@@ -27,5 +27,3 @@ class BoostPtr_ContainerConan(base.BoostBaseConan):
         "boost_unordered",
         "boost_utility"
     ]
-
-
